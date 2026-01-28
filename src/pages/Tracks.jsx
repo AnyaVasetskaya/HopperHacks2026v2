d068a8f1469365d2d120911ad3c72beb390d7461
Anna Vasetskaya <anya.vasetskaya@gmail.com>
2026-01-27 21:13:21 -0500

    tracks

--- src/pages/Tracks.jsx
+++ src/pages/Tracks.jsx
@@ -1,10 +1,13 @@
 import React from 'react';
+import tracksImage from "../assets/tracks.png"
 import './Pages.css';
 
 const Tracks = () => {
   return (
     <div>
-      <p className='section-title'>~Tracks~</p>
+      <p className='section-title'>Tracks</p>
+      <img className='schedule-image' src={tracksImage} alt="wics logo"/>
+
     </div>
   );
 };
