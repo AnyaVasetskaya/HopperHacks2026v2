676f4ff12ff04457d446f4b8d2b39c1daf6db995
JamieDani <daniejamie05@gmail.com>
2026-01-28 03:21:41 -0500

    linky dinky doo

--- vite.config.js
+++ vite.config.js
@@ -3,7 +3,7 @@ import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: '/~wics/hopperhacks/2025/',
+  base: '/~wics/hopperhacks/2026/',
   plugins: [react()],
   assetsInclude: ['**/*.PNG'],
 })
