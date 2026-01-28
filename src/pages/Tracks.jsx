9650ee41a37c38923e15b3d8a60c60d5da791c54
JamieDani <daniejamie05@gmail.com>
2026-01-28 02:35:58 -0500

    stahs

--- src/pages/Tracks.jsx
+++ src/pages/Tracks.jsx
@@ -1,11 +1,14 @@
 import React from 'react';
 import tracksImage from "../assets/tracks.png"
+import quillImage from "../assets/quill.PNG"
 import './Pages.css';
 
 const Tracks = () => {
   return (
-    <div>
+    <div className='tracks-container'>
       <img className='tracks-image' src={tracksImage} alt="wics logo"/>
+      <div className='tracks-blue-box'></div>
+      <img className='tracks-quill' src={quillImage} alt="quill"/>
     </div>
   );
 };
